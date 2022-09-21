10177a05610d589cacfd70975f828baca3b91611
muak1 <muak1ayomide@gmail.com>
2022-09-21 06:02:23 +0100

    my first nextjs project

---
+++ component/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Copyright 2022 muak list</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <>
+    <Head>
+      <title>Ninja list | home</title>
+      <meta name="keywords" content="ninja" />
+    </Head>
+    <div>
+      <h1>about</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore
+        iusto, officiis reiciendis iure voluptatum placeat ratione neque quis
+        blanditiis maxime eveniet voluptates, illo hic incidunt cumque accusamus
+        eius assumenda!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
+        mollitia ratione maiores fugit quaerat ullam quibusdam doloribus rem
+        dolore consectetur eius quos, illo sapiente. Vitae error quaerat aperiam
+        illum modi.
+      </p>
+    </div>
+    </>
+  );
+};
+
+export default about;
